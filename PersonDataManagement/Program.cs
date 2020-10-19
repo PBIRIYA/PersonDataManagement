31afce36290f25e41e68766a02df9c94161c3de9
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-19 21:51:56 +0530

    [Priya] add . Retrieve Selected Records

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -11,7 +11,7 @@ namespace PersonDataManagement
             List<Person> listPersonInCity = new List<Person>();
             AddRecords(listPersonInCity);
             Retrieving_TopTwoRecord_ForAgels_LessThanSixty(listPersonInCity);
-            //CheckingForTeenagerPerson(listPersonInCity);
+            CheckingForTeenagerPerson(listPersonInCity);
         }
         private static void AddRecords(List<Person> listPersonInCity)
         {
@@ -30,6 +30,17 @@ namespace PersonDataManagement
             foreach (Person person in listPersonsInCity.FindAll(e => (e.Age < 60)).Take(2).ToList())
             {
                 Console.WriteLine("Name :" + person.Name + "\t\tAge: " + person.Age);
+            }
+        }
+        public static void CheckingForTeenagerPerson(List<Person> listPersonInCity)
+        {
+            if (listPersonInCity.Any(e => e.Age >= 13 && e.Age < 19))
+            {
+                Console.WriteLine("Yes, We have some teenagers in the list");
+            }
+            else
+            {
+                Console.WriteLine("No, we don't have teenagers in the list");
             }
         }
     }
