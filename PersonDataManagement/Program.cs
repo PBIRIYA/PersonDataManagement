c2250090fb5e1a04caa009dd5252ec7fdbfa6639
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-19 21:42:21 +0530

    [Priya] add . Create List

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -3,30 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 namespace PersonDataManagement
 {
-    class Person
-    {
-        public string SSN;
-        public string Name;
-        public string Address;
-        public int Age;
-        public Person(string ssn, string name, string addr, int age)
-        {
-            SSN = ssn;
-            Name = name;
-            Address = addr;
-            Age = age;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n List of Persons in the City");
             List<Person> listPersonInCity = new List<Person>();
             AddRecords(listPersonInCity);
-            Retrieving_TopTwoRecord_ForAngels_LessThanSixty(listPersonInCity);
-            CheckingForTeenagerPerson(listPersonInCity);
+            //Reterieving_TopTwoRecord_forAgels_LessThanSixty(listPersonInCity);
+            //CheckingForTeenagerPerson(listPersonInCity);
         }
-        //uc1
         private static void AddRecords(List<Person> listPersonInCity)
         {
             listPersonInCity.Add(new Person("203456876", "John", "12 Main Street, Newyork,NY", 15));
@@ -38,26 +24,6 @@ namespace PersonDataManagement
             listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
             listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
             listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
-        }
-        //uc2
-        public static void Retrieving_TopTwoRecord_ForAngels_LessThanSixty(List<Person> listPersonsInCity)
-        {
-            foreach (Person person in listPersonsInCity.FindAll(e => e.Age < 60).Take(2).ToList())
-            {
-                Console.WriteLine("Name" + person.Name + "Age" + person.Age);
-            }
-        }
-        //uc3
-        public static void CheckingForTeenagerPerson(List<Person> listPersonsInCity)
-        {
-            if (listPersonsInCity.Any(e => e.Age >= 13 && e.Age < 19))
-            {
-                Console.WriteLine("Yes");
-            }
-            else
-            {
-                Console.WriteLine("No");
-            }
         }
     }
 }
